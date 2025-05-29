be0657bece05083cd78fed46375f191420aae6d2
yan <h4rd1i@gmail.com>
2025-05-28 21:33:40 -0300

    Refactor Sentry integration in AppModule.
    Reordered imports for ConfigModule initialization and removed the SentryGlobalFilter provider setup. Ensures a cleaner and more streamlined configuration for Sentry in the application.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,15 @@
 import { Module } from '@nestjs/common';
 import { EmailModule } from './email/email.module';
 import { ConfigModule } from '@nestjs/config';
-import { SentryGlobalFilter, SentryModule } from '@sentry/nestjs/setup';
-import { APP_FILTER } from '@nestjs/core';
+import { SentryModule } from '@sentry/nestjs/setup';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
     SentryModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
     EmailModule,
   ],
   controllers: [],
-  providers: [
-    {
-      provide: APP_FILTER,
-      useClass: SentryGlobalFilter,
-    },
-  ],
+  providers: [],
 })
 export class AppModule {}
