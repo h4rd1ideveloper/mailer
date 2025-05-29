7f5869689e9a030db30e328a2b4887cde59b368d
yan <h4rd1i@gmail.com>
2025-05-28 22:34:28 -0300

    Add TLS configuration to email service for secure transport
    Updated the SMTP configuration to include TLS settings, allowing `rejectUnauthorized` to be set to false. This improves compatibility with certain mail servers while ensuring secure connections are established.

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -2,19 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { CreateEmailDto } from './dto/create-email.dto';
 import * as nodemailer from 'nodemailer';
 import { ConfigService } from '@nestjs/config';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
 
 @Injectable()
 export class EmailService {
   private transporter;
 
   constructor(private configService: ConfigService) {
-    const config = {
+    const config: SMTPTransport | SMTPTransport.Options | string = {
       host: this.configService.get('SMTP_HOST'),
       port: parseInt(this.configService.get('SMTP_PORT') as string),
       secure: true,
       auth: {
         user: this.configService.get('SMTP_USER'),
         pass: this.configService.get('SMTP_PASS'),
+      },
+      tls: {
+        rejectUnauthorized: false,
       },
     };
     console.log({ config });
